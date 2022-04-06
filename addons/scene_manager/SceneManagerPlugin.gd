61bd5dfb289895992d4d9ef9d767f05e31aa07ac
Julián Absatz <julianabsatz@gmail.com>
2022-04-06 20:01:40 +0200

    disable broken entity singleton inspector plugin

--- addons/scene_manager/SceneManagerPlugin.gd
+++ addons/scene_manager/SceneManagerPlugin.gd
@@ -4,10 +4,10 @@ var _inspector_plugin
 
 func _enter_tree():
 	add_autoload_singleton("SceneManager", "res://addons/scene_manager/SceneManager.tscn")
-	_inspector_plugin = load("res://addons/scene_manager/NodeFlagsInspectorPlugin.gd").new()
-	add_inspector_plugin(_inspector_plugin)
+	# _inspector_plugin = load("res://addons/scene_manager/NodeFlagsInspectorPlugin.gd").new()
+	# add_inspector_plugin(_inspector_plugin)
 
 
 func _exit_tree():
 	remove_autoload_singleton("SceneManager")
-	remove_inspector_plugin(_inspector_plugin)
+	# remove_inspector_plugin(_inspector_plugin)
